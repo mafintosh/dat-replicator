15d0cc5cd3b4a436095cad10b636eae688eda3ab
Mathias Buus <mathiasbuus@gmail.com>
2014-05-29 20:54:39 +0200

    merged encode/decode into a duplex stream

--- protocol.js
+++ protocol.js
@@ -2,8 +2,24 @@ var stream = require('stream')
 var varint = require('varint')
 var util = require('util')
 
-var Decode = function() {
-  if (!(this instanceof Decode)) return new Decode()
+var pool = new Buffer(5012)
+var used = 0
+var noop = function() {}
+
+var Sink = function(parent, cb) {
+  this._parent = parent
+  if (cb) this.once('finish', cb)
+  stream.Writable.call(this)
+}
+
+util.inherits(Sink, stream.Writable)
+
+Sink.prototype._write = function(data, enc, cb) {
+  this._parent._push(data, cb)
+}
+
+var Protocol = function() {
+  if (!(this instanceof Protocol)) return new Protocol()
 
   var self = this
 
@@ -14,6 +30,7 @@ var Decode = function() {
 
   this._headerPointer = 0
   this._header = new Buffer(50)
+  this._ondrain = null
 
   this._cb = null
   this._nextCalled = false
@@ -25,12 +42,14 @@ var Decode = function() {
     cb()
   }
 
-  stream.Writable.call(this)
+  stream.Duplex.call(this)
 }
 
-util.inherits(Decode, stream.Writable)
+util.inherits(Protocol, stream.Duplex)
 
-Decode.prototype._write = function(data, enc, cb) {
+// decode
+
+Protocol.prototype._write = function(data, enc, cb) {
   if (this._missing) return this._forward(data, cb)
 
   for (var i = 0; i < data.length; i++) {
@@ -47,7 +66,7 @@ Decode.prototype._write = function(data, enc, cb) {
   cb()
 }
 
-Decode.prototype._forward = function(data, cb) {
+Protocol.prototype._forward = function(data, cb) {
   if (data.length > this._missing) {
     var overflow = data.slice(this._missing)
     data = data.slice(0, this._missing)
@@ -71,7 +90,7 @@ Decode.prototype._forward = function(data, cb) {
   this._onstreamdone(cb)
 }
 
-Decode.prototype._onheader = function() {
+Protocol.prototype._onheader = function() {
   this._headerPointer = 0
   this._msbs = 2
 
@@ -92,13 +111,13 @@ Decode.prototype._onheader = function() {
 
 }
 
-Decode.prototype._onstreamdone = function(cb) {
+Protocol.prototype._onstreamdone = function(cb) {
   this._stream = null
   if (this._nextCalled) return cb()
   this._cb = cb
 }
 
-Decode.prototype._onbufferdone = function(cb) {
+Protocol.prototype._onbufferdone = function(cb) {
   var buf = this._buffer
   this._buffer = null
 
@@ -112,7 +131,7 @@ Decode.prototype._onbufferdone = function(cb) {
   }
 }
 
-Decode.prototype._rewrite = function(data, cb) {
+Protocol.prototype._rewrite = function(data, cb) {
   var self = this
   return function(err) {
     if (err) return cb(err)
@@ -120,4 +139,57 @@ Decode.prototype._rewrite = function(data, cb) {
   }
 }
 
-module.exports = Decode
+// encode
+
+Protocol.prototype.meta = function(data, cb) {
+  var buf = new Buffer(JSON.stringify(data))
+  this._header(0, buf.length)
+  this._push(buf, cb || noop)
+}
+
+Protocol.prototype.document = function(doc, cb) {
+  var buf = new Buffer(JSON.stringify(doc))
+  this._header(1, buf.length)
+  this._push(buf, cb || noop)
+}
+
+Protocol.prototype.raw = function(buf, cb) {
+  this._header(2, buf.length)
+  this._push(buf, cb || noop)
+}
+
+Protocol.prototype.blob = function(length, cb) {
+  this._header(3, length)
+  return new Sink(this, cb)
+}
+
+Protocol.prototype._push = function(data, cb) {
+  if (this.push(data)) cb()
+  else this._ondrain = cb
+}
+
+Protocol.prototype._header = function(type, len) {
+  if (pool.length - used < 50) {
+    used = 0
+    pool = new Buffer(5012)
+  }
+
+  var start = used
+
+  varint.encode(type, pool, used)
+  used += varint.encode.bytesWritten
+
+  varint.encode(len, pool, used)
+  used += varint.encode.bytesWritten
+
+  this.push(pool.slice(start, used))
+}
+
+Protocol.prototype._read = function() {
+  if (!this._ondrain) return
+  var ondrain = this._ondrain
+  this._ondrain = null
+  ondrain()
+}
+
+module.exports = Protocol
