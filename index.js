e1d248baff7bb499b70b05ab29607b9c5ca48207
Mathias Buus <mathiasbuus@gmail.com>
2014-07-17 11:46:08 +0200

    support for debug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var protocol = require('dat-replication-protocol')
 var through = require('through2')
 var request = require('request')
+var debug = require('debug')('dat-replicator')
 
 var STREAM_OPTS = {highWaterMark:16}
 
@@ -33,6 +34,7 @@ var replication = function(dat) {
         if (err) return cb(err)
         if (!keys.length) return cb()
         var bl = attachments[keys.shift()]
+        debug('send.blob')
         dat.blobs.createReadStream(bl.hash).pipe(p.blob(bl.size, loop))
       }
 
@@ -41,6 +43,7 @@ var replication = function(dat) {
 
     var writeNoBlobs = function(doc, enc, cb) {
       var data = doc.value || doc.data
+      debug('send.document', data)
       p.document(data, cb)
     }
 
@@ -48,11 +51,13 @@ var replication = function(dat) {
       var data = doc.value || doc.data
       writeAttachments(data.attachments, function(err) {
         if (err) return cb(err)
+        debug('send.document', data)
         p.document(data, cb)
       })
     }
 
     var flush = function(cb) {
+      debug('send.finalize')
       p.finalize(cb)
     }
 
@@ -62,6 +67,7 @@ var replication = function(dat) {
         data: true
       })
 
+      debug('receive.meta', meta)
       rs.pipe(through.obj(STREAM_OPTS, meta.blobs === false ? writeNoBlobs : write, flush))
     }
 
@@ -77,6 +83,7 @@ var replication = function(dat) {
 
     // not sure about this but this is a start...
     p.on('conflict', function(conflict, cb) {
+      debug('receive.conflict', conflict)
       var err = new Error(conflict.message || 'conflict')
       err.conflict = true
       err.key = conflict.key
@@ -95,11 +102,14 @@ var replication = function(dat) {
     var ws = dat.createWriteStream({objects:true})
 
     if (opts.meta !== false) {
-      p.meta({
+      var meta = {
         blobs: opts.blobs,
         change: dat.storage.change,
         schema: dat.schema.toJSON()
-      })
+      }
+
+      debug('send.meta', meta)
+      p.meta(meta)
     }
 
     ws.on('error', function(err) {
@@ -107,18 +117,22 @@ var replication = function(dat) {
     })
     
     ws.on('conflict', function(conflict) {
+      debug('send.conflict', conflict)
       p.conflict({message:conflict.message, key:conflict.key, version:conflict.version})
     })
 
     p.on('blob', function(blob, cb) {
+      debug('receive.blob')
       blob.pipe(dat.blobs.createWriteStream(cb))
     })
 
     p.on('document', function(doc, cb) {
+      debug('receive.document', doc)
       ws.write(doc, cb)
     })
 
     p.on('finalize', function(cb) {
+      debug('receive.finalize')
       ws.on('end', cb)
       ws.end()
     })
