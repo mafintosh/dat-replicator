356c3ee42cc0b4ec847a57a52038a339e1b38482
Mathias Buus <mathiasbuus@gmail.com>
2014-08-02 23:02:16 +0200

    wip - support for level-dat + store

--- index.js
+++ index.js
@@ -1,144 +1,71 @@
-var protocol = require('dat-replication-protocol')
 var through = require('through2')
-var request = require('request')
-var debug = require('debug')('dat-replicator')
+var protocol = require('dat-replication-protocol')
+var pump = require('pump')
 
-var replication = function(dat) {
+var noop = function() {}
+
+module.exports = function(db, store) {
   var that = {}
 
-  that.createPullStream = function(remote, opts) {
-    var rcvd = that.receive(opts)
-    var req = request.post(remote+'/api/replicator/send')
-    req.pipe(rcvd).pipe(req)
-    return rcvd
-  }
+  that.receive = function() {
+    var blobs = 0
+    var next = noop
 
-  that.createPushStream = function(remote, opts) {
-    var send = that.send(opts)
-    var req = request.post(remote+'/api/replicator/receive')
-    req.pipe(send).pipe(req)
-    return send
+    var onblob = function(stream) {
+      blobs++
+      pump(stream, store.createWriteStream(), function() {
+        if (--blobs) return
+        next()
+        next = noop
+      })
+    }
+
+    var ondrain = function(cb) {
+      if (!blobs) cb()
+      else next = cb
+    }
+
+    var readBlobs = function(data, enc, cb) {
+      ondrain(function() {
+        cb(null, data)
+      })
+    }
+
+    var onchanges = function(stream) {
+      pump(stream, through.obj(readBlobs), db.createChangesWriteStream({valueEncoding:'binary'}))
+    }
+
+    return protocol(function(type, stream) {
+      if (type === protocol.CHANGES) onchanges(stream)
+      if (type === protocol.BLOB) onblob(stream)
+    })
   }
 
   that.send = function(opts) {
     if (!opts) opts = {}
-    var p = protocol()
-
-    var writeAttachments = function(attachments, cb) {
-      if (!attachments) return cb()
-
-      var keys = Object.keys(attachments)
-      var loop = function(err) {
-        if (err) return cb(err)
-        if (!keys.length) return cb()
-        var bl = attachments[keys.shift()]
-        debug('send.blob')
-        dat.blobs.createReadStream(bl.hash).pipe(p.blob(bl.size, loop))
-      }
-
-      loop()
-    }
-
-    var writeNoBlobs = function(doc, enc, cb) {
-      var data = doc.value || doc.data
-      debug('send.document', data)
-      p.document(data, cb)
-    }
-
-    var write = function(doc, enc, cb) {
-      var data = doc.value || doc.data
-      writeAttachments(data[opts.blobKey || 'blobs'], function(err) {
-        if (err) return cb(err)
-        debug('send.document', data)
-        p.document(data, cb)
-      })
-    }
-
-    var flush = function(cb) {
-      debug('send.finalize')
-      p.finalize(cb)
-    }
-
-    var ready = function(meta) {
-      var rs = dat.createChangesStream({
-        since: meta.change,
-        data: true
-      })
-
-      debug('receive.meta', meta)
-      rs.pipe(through.obj(meta.blobs === false ? writeNoBlobs : write, flush))
-    }
-
-    if (opts.meta) ready(opts.meta)
-    else {
-      p.once('meta', function(meta, cb) {
-        ready(meta)
-        cb()
-      })
-    }
-
-    if (opts.ping !== false) p.ping() // support more transports by forcing a flush (looking at you http)
-
-    // not sure about this but this is a start...
-    p.on('conflict', function(conflict, cb) {
-      debug('receive.conflict', conflict)
-      var err = new Error(conflict.message || 'conflict')
-      err.conflict = true
-      err.key = conflict.key
-      err.version = conflict.version
-      p.emit('error', err)
-      cb()
-    })
-
-    return p
-  }
-
-  that.receive = function(opts) {
-    if (!opts) opts = {}
 
     var p = protocol()
-    var ws = dat.createWriteStream({objects:true})
 
-    if (opts.meta !== false) {
-      var meta = {
-        blobs: opts.blobs,
-        change: dat.storage.change,
-        schema: dat.schema.toJSON()
-      }
+    var writeBlobs = function(data, enc, cb) {
+      if (data.subset !== 'blobs') return cb(null, data)
 
-      debug('send.meta', meta)
-      p.meta(meta)
+      var metadata = JSON.parse(data.value.toString())
+      var last = metadata[metadata.length-1]
+      var bl = store.createReadStream(last.hash)
+
+      pump(bl, p.createBlobStream(last.length)).on('finish', function() {
+        cb(null, data)
+      })
     }
 
-    ws.on('error', function(err) {
-      p.emit('error', err)
-    })
-    
-    ws.on('conflict', function(conflict) {
-      debug('send.conflict', conflict)
-      p.conflict({message:conflict.message, key:conflict.key, version:conflict.version})
-    })
-
-    p.on('blob', function(blob, cb) {
-      debug('receive.blob')
-      blob.pipe(dat.blobs.createWriteStream(cb))
-    })
-
-    p.on('document', function(doc, cb) {
-      debug('receive.document', doc)
-      ws.write(doc, cb)
-    })
-
-    p.on('finalize', function(cb) {
-      debug('receive.finalize')
-      ws.on('end', cb)
-      ws.end()
-    })
+    pump(
+      db.createChangesReadStream({since:opts.since, valueEncoding:'binary', data:true}),
+      through.obj(writeBlobs),
+      p.createChangesStream()
+    )
 
     return p
   }
 
   return that
 }
-
-module.exports = replication
